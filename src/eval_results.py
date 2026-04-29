cb9fd8b48a1237817422b378e0e4a967745bd691
Abel Schootemeijer <abelschootemeijer@gmail.com>
2026-04-29 16:05:00 +0200

    Updated scripts in src folder

--- src/eval_results.py
+++ src/eval_results.py
@@ -1,5 +1,50 @@
 import numpy as np
+import pandas as pd
 from scipy import stats
+from scipy.optimize import newton
+
+
+def calc_xirr( dates, cashflows ):
+    """
+    (X)IRR: Internal Rate of Return (or annual return)
+    XNPV:   eXtended Net Present Value. Essentially uses time difference between cashflows.
+    The correct rate (annual return) is the one for which the sum of the XNPV is zero
+    """
+    dates = pd.to_datetime( dates )
+    start_date = min(dates)
+    print( '\nDATES',dates )
+    print( '\nDAYS', (dates-start_date).days )
+    print( '\nCASHFLOWS', cashflows )
+    def xnpv(rate, cashflows, dates):
+        start_date = min(dates)
+        return sum([cf / (1 + rate)**((d - start_date).days / 365.25) for cf, d in zip(cashflows, dates)])
+    # Newton-Raphson method to find rate at which XNPV is 0
+    try:
+        return np.round(newton(lambda r: xnpv(r, cashflows, dates), 0.1) * 100, 1)
+    except RuntimeError:
+        return 0.0
+
+
+def _kutversie_calc_xirr( dates, cashflows ):
+    """
+    (X)IRR: Internal Rate of Return (or annual return)
+    XNPV:   eXtended Net Present Value. Essentially uses time difference between cashflows.
+    The correct rate (annual return) is the one for which the sum of the XNPV is zero
+    """
+    dates = pd.to_datetime( dates )
+    days_from_start = ( dates - min(dates) ).days.tolist()
+    print( 'DATES\n',dates )
+    print( 'DAYS\n', days_from_start )
+    #print( 'DAYS\n', (dates - start_date).days )
+
+    def xnpv(rate, cashflows, days_from_start_f):
+        #start_date = min(dates)
+        return sum([cf / (1 + rate)**( d / 365.25) for cf, d in zip(cashflows, days_from_start_f)])
+    # Newton-Raphson method to find rate at which XNPV is 0
+    try:
+        return np.round(newton(lambda r: xnpv(r, cashflows, days_from_start), 0.1) * 100, 1)
+    except RuntimeError:
+        return 0.0
 
 def calc_avg_yrly_incr(periods, value_rats):
     if not periods: return 0, 0
